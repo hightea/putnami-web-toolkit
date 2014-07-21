1d5cd4e45a6ac85674a85ec2b98d1b9a86b1ec03
Fabien DUMAY <fabien.dumay@gmail.com>
2014-07-21 11:11:07 +0200

    [license] Use the same license header within all sub projects

--- doc/src/main/java/fr/putnami/pwt/doc/client/guide/ReferenceGuide.java
+++ doc/src/main/java/fr/putnami/pwt/doc/client/guide/ReferenceGuide.java
@@ -1,18 +1,18 @@
 /**
- * This file is part of pwt-doc.
+ * This file is part of pwt.
  *
- * pwt-doc is free software: you can redistribute it and/or modify
+ * pwt is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * pwt-doc is distributed in the hope that it will be useful,
+ * pwt is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with pwt-doc.  If not, see <http://www.gnu.org/licenses/>.
+ * along with pwt.  If not, see <http://www.gnu.org/licenses/>.
  */
 package fr.putnami.pwt.doc.client.guide;
 
@@ -143,7 +143,7 @@ public class ReferenceGuide extends Composite implements EntryPoint {
 		initWidget(Binder.BINDER.createAndBindUi(this));
 
 		Theme theme = new Theme();
-		theme.addLink(new CssLink("theme/doc/style/pwt-doc.css", 0));
+		theme.addLink(new CssLink("theme/doc/style/pwt.css", 0));
 		ThemeController.get().installTheme(theme);
 
 		SimpleErrorDisplayer errorDisplayer = new SimpleErrorDisplayer();
--- doc/src/main/java/fr/putnami/pwt/doc/client/page/bootstrap/BootstrapView.java
+++ doc/src/main/java/fr/putnami/pwt/doc/client/page/bootstrap/BootstrapView.java
@@ -1,18 +1,18 @@
 /**
- * This file is part of pwt-doc.
+ * This file is part of pwt.
  *
- * pwt-doc is free software: you can redistribute it and/or modify
+ * pwt is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * pwt-doc is distributed in the hope that it will be useful,
+ * pwt is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with pwt-doc.  If not, see <http://www.gnu.org/licenses/>.
+ * along with pwt.  If not, see <http://www.gnu.org/licenses/>.
  */
 package fr.putnami.pwt.doc.client.page.bootstrap;
 
@@ -42,7 +42,7 @@ public class BootstrapView extends Page<BootstrapPlace> {
 	public BootstrapView() {
 		yeti.addLink(new CssLink("theme/yeti/style/bootstrap-yeti.min.css", 0));
 		amelia.addLink(new CssLink("theme/amelia/style/bootstrap-amelia.min.css", 0));
-		doc.addLink(new CssLink("theme/doc/style/pwt-doc.css", 0));
+		doc.addLink(new CssLink("theme/doc/style/pwt.css", 0));
 	}
 
 	@Override
