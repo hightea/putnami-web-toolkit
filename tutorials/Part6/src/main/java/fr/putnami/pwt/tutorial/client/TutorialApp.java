c91aa1ef555cfc44ebabaf32e09917b8b5d97fa4
Fabien DUMAY <fabien.dumay@gmail.com>
2014-08-29 18:44:36 +0200

    [doc][tuto] Part6 pwt 1.1 migration

--- tutorials/Part6/src/main/java/fr/putnami/pwt/tutorial/client/TutorialApp.java
+++ tutorials/Part6/src/main/java/fr/putnami/pwt/tutorial/client/TutorialApp.java
@@ -1,8 +1,10 @@
 package fr.putnami.pwt.tutorial.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
 
+import fr.putnami.pwt.core.mvp.client.ActivityFactory;
 import fr.putnami.pwt.core.mvp.client.MvpController;
 import fr.putnami.pwt.core.theme.client.CssLink;
 import fr.putnami.pwt.core.theme.client.IconFont;
@@ -18,8 +20,10 @@ public class TutorialApp implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		PageDecorator display = new PageDecorator();
+		PageDecorator display = GWT.create(PageDecorator.class);
 		RootPanel.get().add(display);
+
+		MvpController controller = MvpController.get();
 
 		Theme theme = new Theme();
 		theme.addLink(new CssLink("theme/tutorial/style/pwt-tutorial.css", 0));
@@ -31,14 +35,13 @@ public class TutorialApp implements EntryPoint {
 		theme.setIconFont(font);
 		ThemeController.get().installTheme(theme);
 
-		MvpController controller = MvpController.get();
 		controller.setDisplay(display);
 
-		controller.setDefaultPlace(WelcomePlace.INSTANCE);
-		controller.registerActivity(WelcomePlace.INSTANCE);
-		controller.registerActivity(AboutPlace.INSTANCE);
-		controller.registerActivity(ContactPlace.INSTANCE);
-		controller.registerActivity(IssuesPlace.INSTANCE);
+		controller.setDefaultPlace(new WelcomePlace());
+		controller.registerActivity((ActivityFactory) GWT.create(WelcomePlace.class));
+		controller.registerActivity((ActivityFactory) GWT.create(AboutPlace.class));
+		controller.registerActivity((ActivityFactory) GWT.create(ContactPlace.class));
+		controller.registerActivity((ActivityFactory) GWT.create(IssuesPlace.class));
 
 		// Handles Current token to navigate to the correct place
 		controller.handleCurrentHistory();
