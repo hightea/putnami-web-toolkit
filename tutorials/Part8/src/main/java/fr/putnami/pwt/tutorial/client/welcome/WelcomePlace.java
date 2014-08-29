57321d16937b6471d96ba2d82a28dd4822ff13b8
Fabien DUMAY <fabien.dumay@gmail.com>
2014-08-29 22:31:28 +0200

    [doc][tuto] Part8 pwt 1.1 migration

--- tutorials/Part8/src/main/java/fr/putnami/pwt/tutorial/client/welcome/WelcomePlace.java
+++ tutorials/Part8/src/main/java/fr/putnami/pwt/tutorial/client/welcome/WelcomePlace.java
@@ -1,20 +1,8 @@
 package fr.putnami.pwt.tutorial.client.welcome;
 
-import com.google.gwt.core.client.GWT;
+import fr.putnami.pwt.core.mvp.client.ViewPlace;
+import fr.putnami.pwt.core.mvp.client.annotation.ActivityDescription;
 
-import fr.putnami.pwt.core.mvp.client.MvpPlace;
-import fr.putnami.pwt.core.mvp.client.ViewProxy;
-
-public class WelcomePlace extends MvpPlace {
-
-	public static final WelcomePlace INSTANCE = new WelcomePlace();
-
-	public WelcomePlace() {
-		super((ViewProxy) GWT.create(WelcomeView.class), null);
-	}
-
-	@Override
-	public MvpPlace getPlace(String token) {
-		return WelcomePlace.INSTANCE;
-	}
+@ActivityDescription(view = WelcomeView.class)
+public class WelcomePlace extends ViewPlace {
 }
