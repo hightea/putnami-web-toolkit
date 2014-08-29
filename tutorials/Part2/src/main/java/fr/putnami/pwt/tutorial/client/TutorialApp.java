0409107874fb360bb2d477c58eabda6bf3514d72
Fabien DUMAY <fabien.dumay@gmail.com>
2014-08-29 17:20:16 +0200

    [doc][tuto] Part2 pwt 1.1 migration

--- tutorials/Part2/src/main/java/fr/putnami/pwt/tutorial/client/TutorialApp.java
+++ tutorials/Part2/src/main/java/fr/putnami/pwt/tutorial/client/TutorialApp.java
@@ -1,8 +1,10 @@
 package fr.putnami.pwt.tutorial.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
 
+import fr.putnami.pwt.core.mvp.client.ActivityFactory;
 import fr.putnami.pwt.core.mvp.client.MvpController;
 import fr.putnami.pwt.core.widget.client.OneWidgetPanel;
 import fr.putnami.pwt.tutorial.client.welcome.WelcomePlace;
@@ -17,8 +19,8 @@ public class TutorialApp implements EntryPoint {
 		MvpController controller = MvpController.get();
 		controller.setDisplay(display);
 
-		controller.setDefaultPlace(WelcomePlace.INSTANCE);
-		controller.registerActivity(WelcomePlace.INSTANCE);
+		controller.setDefaultPlace(new WelcomePlace());
+		controller.registerActivity((ActivityFactory) GWT.create(WelcomePlace.class));
 
 		// Handles Current token to navigate to the correct place
 		controller.handleCurrentHistory();
